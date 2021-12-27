7ca88d05f15b15291105525a5d08fe3da1c9834e
saliksian <salikafzal91@gmail.com>
2021-12-27 16:55:39 +0500

    Program3
    Concatenate Two Strings

---
+++ Program3/script.js
@@ -0,0 +1,8 @@
+var string1 = "Muhammad Salik";
+var string2 = "Computer Science";
+
+var concateStrings = string1.concat(" ",string2)
+
+console.log("First string:", string1);
+console.log("Second string:", string2);
+console.log("Concatenate Strings:", concateStrings);
